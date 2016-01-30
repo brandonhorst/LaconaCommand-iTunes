f6b1525fccef814bf03d95496c988fe0e43cfa75
Brandon Horst <brandonhorst@gmail.com>
2016-01-29 20:27:57 -0500

    Initial beta launch

--- src/index.jsx
+++ src/index.jsx
@@ -1,140 +1,143 @@
 /** @jsx createElement */
 import _ from 'lodash'
-import {createElement, Phrase, Source} from 'lacona-phrase'
-import {Applescript} from 'lacona-command-osx'
+import { createElement, Phrase, Source } from 'lacona-phrase'
+import { isDemo, fetchMusic, playSongIds, musicPlay, musicPause, musicNext, musicPrevious, musicStop } from 'lacona-api'
+import { Command } from 'lacona-command'
 
-export function executeSpecific (result) {
-  const ids = _.chain(result.music).map('ids').flatten().value()
-
-  if (ids) {
-    const command = `
-      tell application "iTunes"
-      	if user playlist "Lacona Playlist" exists then
-      		try
-      			delete user playlist "Lacona Playlist"
-      		end try
-      	end if
-
-    		make new user playlist with properties {name:"Lacona Playlist", shuffle: false}
-
-      	repeat with tid in {${results.ids.join(',')}}
-      		set trk to (some track of first user playlist whose database ID is tid)
-      		duplicate trk to end of user playlist "Lacona Playlist"
-      	end repeat
-
-        play user playlist "Lacona Playlist"
-      end tell
-    `
-    global.applescript(command)
+function andify (array) {
+  if (array.length === 1) {
+    return array
+  } else {
+    return _.chain(array)
+      .slice(0, -2)
+      .map(item => [item, {text: ', '}])
+      .flatten()
+      .concat(_.slice(array, -2, -1)[0])
+      .concat({text: ' and '})
+      .concat(_.slice(array, -1)[0])
+      .value()
   }
 }
 
-const script = 'tell application "iTunes" to get {name, album, artist, album artist, composer, genre, year, disc number, track number, database ID} of every track of first library playlist'
+class MusicObject {
+  constructor ({music}) {
+    this.music = music
+  }
 
-// class Sorted extends Source {
-//   source () {
-//     return {data: this.props.children[0]}
-//   }
-//
-//   onCreate () {
-//     this.replaceData([])
-//   }
-//
-//   trigger () {
-//     this.sources.data.trigger()
-//   }
-//
-//   onUpdate () {
-//     const sortedData = _.sortByAll(this.sources.data.data, this.props.keys)
-//     this.replaceData(sortedData)
-//   }
-// }
+  _demoExecute () {
+    const descriptions = _.map(this.music, ({song, album, artist, genre, playlist, composer}) => {
+      if (song) {
+        return {text: song, argument: 'song'}
+      } else if (album) {
+        return [{text: 'all songs on '}, {text: album, argument: 'album'}]
+      } else if (artist) {
+        return [{text: 'all songs by '}, {text: artist, argument: 'artist'}]
+      } else if (genre) {
+        return [{text: 'all songs in the '}, {text: genre, argument: 'genre'}, {text: 'genre'}]
+      } else if (playlist) {
+        return [{text: 'all songs in the '}, {text: playlist, argument: 'playlist'}, {text: 'playlist'}]
+      } else if (composer) {
+        return [{text: 'all songs composed by '}, {text: composer, argument: 'composer'}]
+      }
+    })
 
-function artistify({albumArtist, artist}) {
-  return albumArtist || artist
+    return _.flattenDeep([
+      {text: 'play ', category: 'action'},
+      andify(descriptions),
+      {text: ' in ', category: 'conjunction'},
+      {text: ' iTunes', argument: 'application'}
+    ])
+  }
+
+  execute () {
+    const ids = _.chain(this.music).map('ids').flatten().value()
+    playSongIds({ids})
+  }
 }
 
-function sortInteger(key) {
-  return item => parseInt(item[key], 10)
-}
+class Music extends Source {
+  data = []
 
-function arrangeMusic(arrays) {
-  return _.chain(arrays)
-    .thru(_.spread(_.zip))
-    .map(_.partial(_.zipObject, ['name', 'album', 'artist', 'albumArtist', 'composer', 'genre', 'year', 'discNumber', 'trackNumber', 'id']))
-    .sortByAll([artistify, sortInteger('year'), 'album', sortInteger('discNumber'), sortInteger('trackNumber'), sortInteger('id')])
-    .value()
-}
-
-class DemoPlay extends Source {
   onCreate () {
-    this.replaceData(global.config.music)
-  }
-
-  trigger () {}
-}
-
-class PlaySpecific extends Phrase {
-  source () {
-    if (process.env.LACONA_ENV === 'demo') {
-      return {
-        music: <DemoPlay />
-      }
-    } else {
-      return {
-        music: (
-          <thru function={arrangeMusic}>
-            <Applescript code={script} fetchOn='triggerOnce' />
-          </thru>
-        )
-      }
+    if (isDemo()) {
+      this.fetch()
     }
   }
 
+  fetch () {
+    fetchMusic((err, music) => {
+      if (err) {
+        console.error(err)
+      } else {
+        this.setData(music)
+      }
+    })
+  }
+
+  onActivate () {
+    this.setData([])
+  }
+
+  trigger () {
+    if (_.isEmpty(this.data)) {
+      this.fetch()
+    }
+  }
+}
+
+class PlaySpecific extends Phrase {
+  static extends = [Command]
+
+  observe () {
+    return <Music />
+  }
+
   describe () {
-    const tracks = _.map(this.sources.music.data, ({name, id}) => ({text: name, value: {song: name, ids: [id]}}))
-    const albums = _.chain(this.sources.music.data)
+    const tracks = _.map(this.source.data, ({name, id}) => ({text: name, value: {song: name, ids: [id]}}))
+    const albums = _.chain(this.source.data)
       .groupBy('album')
       .map((albumItems, album) => ({text: album, value: {album, ids: _.map(albumItems, 'id')}}))
       .value()
-    const artists = _.chain(this.sources.music.data)
-      .groupBy(artistify)
+    const artists = _.chain(this.source.data)
+      .groupBy('artist')
       .map((artistItems, artist) => ({text: artist, value: {artist, ids: _.map(artistItems, 'id')}}))
       .value()
-    const genres = _.chain(this.sources.music.data)
+    const genres = _.chain(this.source.data)
       .groupBy('genre')
       .map((genreItems, genre) => ({text: genre, value: {genre, ids: _.map(genreItems, 'id')}}))
       .value()
-    const composers = _.chain(this.sources.music.data)
+    const composers = _.chain(this.source.data)
       .groupBy('composer')
       .map((composerItems, composer) => ({text: composer, value: {composer, ids: _.map(composerItems, 'id')}}))
       .value()
 
     return (
-      <sequence>
-        <literal text='play ' category='action' />
-        <descriptor id='music' trigger={this.sources.music.trigger.bind(this.sources.music)}>
-          <repeat unique separator={<list items={[' and ', ', ', ', and ']} limit={1} />}>
-            <choice>
-              <argument text='song'>
-                <list items={tracks} fuzzy={true} limit={10} />
-              </argument>
-              <argument text='album'>
-                <list items={albums} fuzzy={true} limit={10} />
-              </argument>
-              <argument text='artist'>
-                <list items={artists} fuzzy={true} limit={10} />
-              </argument>
-              <argument text='genre'>
-                <list items={genres} fuzzy={true} limit={10} />
-              </argument>
-              <argument text='composer'>
-                <list items={composers} fuzzy={true} limit={10} />
-              </argument>
-            </choice>
-          </repeat>
-        </descriptor>
-      </sequence>
+      <map function={result => new MusicObject(result)}>
+        <sequence>
+          <literal text='play ' category='action' />
+          <tap id='music' function={this.source.trigger.bind(this.source)}>
+            <repeat unique separator={<list items={[' and ', ', ', ', and ']} limit={1} />}>
+              <choice>
+                <label text='song'>
+                  <list items={tracks} fuzzy={true} limit={10} />
+                </label>
+                <label text='album'>
+                  <list items={albums} fuzzy={true} limit={10} />
+                </label>
+                <label text='artist'>
+                  <list items={artists} fuzzy={true} limit={10} />
+                </label>
+                <label text='genre'>
+                  <list items={genres} fuzzy={true} limit={10} />
+                </label>
+                <label text='composer'>
+                  <list items={composers} fuzzy={true} limit={10} />
+                </label>
+              </choice>
+            </repeat>
+          </tap>
+        </sequence>
+      </map>
     )
   }
 
@@ -146,52 +149,98 @@ class PlaySpecific extends Phrase {
   // }
 }
 
-function executeControl (result) {
-  if (result.verb === 'play') {
-    global.applescript('tell application "iTunes" to play')
-  } else if (result.verb === 'next') {
-    global.applescript('tell application "iTunes" to next track')
-  } else if (result.verb === 'previous') {
-    global.applescript('tell application "iTunes" to previous track')
-  } else if (result.verb === 'pause') {
-    global.applescript('tell application "iTunes" to pause')
-  } else if (result.verb === 'stop') {
-    global.applescript('tell application "iTunes" to stop')
+class ControlObject {
+  constructor ({verb}) {
+    this.verb = verb
+  }
+
+  _demoExecute () {
+    if (this.verb === 'play') {
+      return [
+        {text: 'play ', category: 'action'},
+        {text: 'the current song', category: 'argument5'},
+        {text: ' in '},
+        {text: 'iTunes', argument: 'application'}
+      ]
+    } else if (this.verb === 'next') {
+      return [
+        {text: 'play ', category: 'action'},
+        {text: 'the next song', category: 'argument5'},
+        {text: ' in '},
+        {text: 'iTunes', argument: 'application'}
+      ]
+
+    } else if (this.verb === 'previous') {
+      return [
+        {text: 'play ', category: 'action'},
+        {text: 'the previous song', category: 'argument5'},
+        {text: ' in '},
+        {text: 'iTunes', argument: 'application'}
+      ]
+
+    } else if (this.verb === 'stop') {
+      return [
+        {text: 'stop playing music ', category: 'action'},
+        {text: ' in '},
+        {text: 'iTunes', argument: 'application'}
+      ]
+
+    } else if (this.verb === 'pause') {
+      return [
+        {text: 'pause ', category: 'action'},
+        {text: 'the current song', category: 'argument5'},
+        {text: ' in '},
+        {text: 'iTunes', argument: 'application'}
+      ]
+    }
+  }
+
+  execute () {
+    if (this.verb === 'play') {
+      musicPlay()
+    } else if (this.verb === 'next') {
+      musicNext()
+    } else if (this.verb === 'previous') {
+      musicPrevious()
+    } else if (this.verb === 'pause') {
+      musicPause()
+    } else if (this.verb === 'stop') {
+      musicStop()
+    }
   }
 }
 
 class Control extends Phrase {
+  static extends = [Command]
+
   describe () {
     return (
-      <choice>
-        <sequence>
-          <literal text='play ' category='action' />
-          <choice merge={true} id='verb' limit={2}>
-            <list items={['current song', 'current track', 'music']} value='play' limit={1} category='action' />
-            <list items={['previous song', 'previous track', 'last song', 'last track']} value='previous' limit={1} category='action' />
-            <list items={['next song', 'next track']} value='next' limit={1} category='action' />
-          </choice>
-        </sequence>
-        <sequence value={{verb: 'pause'}}>
-          <literal text='pause ' category='action' />
-          <list items={['current song', 'current track', 'music']} limit={1} category='action' />
-        </sequence>
-        <sequence value={{verb: 'next'}}>
-          <literal text='skip ' category='action' />
-          <list items={['current song', 'current track']} limit={1} category='action' />
-        </sequence>
-        <sequence value={{verb: 'stop'}}>
-          <literal text='stop ' category='action' />
-          <list items={['current song', 'current track']} limit={1} category='action' />
-        </sequence>
-      </choice>
+      <map function={result => new ControlObject(result)}>
+        <choice>
+          <sequence>
+            <literal text='play ' category='action' />
+            <choice merge={true} id='verb' limit={2}>
+              <list items={['current song', 'current track', 'music']} value='play' limit={1} category='action' />
+              <list items={['previous song', 'previous track', 'last song', 'last track']} value='previous' limit={1} category='action' />
+              <list items={['next song', 'next track']} value='next' limit={1} category='action' />
+            </choice>
+          </sequence>
+          <sequence value={{verb: 'pause'}}>
+            <literal text='pause ' category='action' />
+            <list items={['current song', 'current track', 'music']} limit={1} category='action' />
+          </sequence>
+          <sequence value={{verb: 'next'}}>
+            <literal text='skip ' category='action' />
+            <list items={['current song', 'current track']} limit={1} category='action' />
+          </sequence>
+          <sequence value={{verb: 'stop'}}>
+            <literal text='stop ' category='action' />
+            <list items={['current song', 'current track']} limit={1} category='action' />
+          </sequence>
+        </choice>
+      </map>
     )
   }
 }
 
-export default {
-  sentences: [
-    {Sentence: PlaySpecific, execute: executeSpecific},
-    {Sentence: Control, execute: executeControl}
-  ]
-}
+export const extensions = [PlaySpecific, Control]
